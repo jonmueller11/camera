3b0bb803f3e2d17d464d28b3ce08b271d88f5ae0
jonmueller11 <jonas1999mueller@gmail.com>
2022-03-12 16:40:40 +0100

    .

--- main_module.py
+++ main_module.py
@@ -2,10 +2,10 @@ import control
 import image_processing
 import time
 
-PATH= "/Pictures/temp/bild.jpg"
+PATH= "Pictures/temp/bild.jpg"
 
 for i in range(10):
     control.take_picture(PATH)
     time.sleep(3)
     pic = image_processing.open_picture(PATH)
-    control.save_pictures(pic, "/Pictures")
+    control.save_pictures(pic, "Pictures")
